cd352107858afdfd597352ead082eeee20150d3b
Ardak <98506975+ArdakUtepbergenov@users.noreply.github.com>
2026-05-22 18:18:06 +0500

    Update main.js

--- main.js
+++ main.js
@@ -159,34 +159,3 @@ document.querySelectorAll('img[loading="lazy"]').forEach(img => {
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  document.querySelectorAll('.services-open-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const modal = document.getElementById(btn.dataset.modal);
-
-      if (modal) {
-        modal.classList.add('active');
-      }
-    });
-  });
-
-  document.querySelectorAll('.services-close').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const modal = btn.closest('.services-modal');
-
-      if (modal) {
-        modal.classList.remove('active');
-      }
-    });
-  });
-
-  document.querySelectorAll('.services-modal').forEach(modal => {
-    modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
-        modal.classList.remove('active');
-      }
-    });
-  });
-
-});
